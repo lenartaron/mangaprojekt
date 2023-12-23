05219a5ff210a243fe220218d803c0e5ec97f72f
lenartaron <allium.szinkron@gmail.com>
2023-12-23 14:20:42 +0100

    Update registration.js

--- registration/registration.js
+++ registration/registration.js
@@ -1,4 +1,3 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
 
